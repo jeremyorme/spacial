788c89a9a5c1539bc4413d807970d28dd211f0a4
Jeremy Orme <jeremy@heobe.com>
2021-02-01 15:34:27 +0000

    Add basic geometry classes

---
+++ spacial/geometry.py
@@ -0,0 +1,160 @@
+from typing import Iterable
+from math import sqrt
+
+
+# represents an 2-dimensional vector
+class Vector:
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other: 'Vector'):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: 'Vector'):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __neg__(self):
+        return Vector(-self.x, -self.y)
+
+    def __mul__(self, factor: float):
+        return Vector(self.x * factor, self.y * factor)
+
+    def __truediv__(self, factor: float):
+        return Vector(self.x / factor, self.y / factor)
+
+    def equals(self, other: 'Vector', tolerance: float):
+        return abs(self.x - other.x) <= tolerance and abs(self.y - other.y) < tolerance
+
+    def dot(self, other: 'Vector'):
+        return self.x * other.x + self.y * other.y
+
+    def magnitude(self):
+        return sqrt(self.dot(self))
+
+
+# represents a matrix with two rows (i, j) and two columns
+class Matrix:
+    def __init__(self, ix: float, iy: float, jx: float, jy: float):
+        self.ix = ix
+        self.iy = iy
+        self.jx = jx
+        self.jy = jy
+
+    def __eq__(self, other: 'Matrix'):
+        return self.ix == other.ix and self.iy == other.iy and self.jx == other.jx and self.jy == other.jy
+
+    def __add__(self, other: 'Matrix'):
+        return Matrix(self.ix + other.ix, self.iy + other.iy, self.jx + other.jx, self.jy + other.jy)
+
+    def __sub__(self, other: 'Matrix'):
+        return Matrix(self.ix - other.ix, self.iy - other.iy, self.jx - other.jx, self.jy - other.jy)
+
+    def __neg__(self):
+        return Matrix(-self.ix, -self.iy, -self.jx, -self.jy)
+
+    def __mul__(self, factor: float):
+        return Matrix(self.ix * factor, self.iy * factor, self.jx * factor, self.jy * factor)
+
+    def __truediv__(self, factor: float):
+        return Matrix(self.ix / factor, self.iy / factor, self.jx / factor, self.jy / factor)
+
+    def multiply_i(self, v: Vector):
+        return self.ix * v.x + self.iy * v.y
+
+    def multiply_j(self, v: Vector):
+        return self.jx * v.x + self.jy * v.y
+
+    def multiply(self, v: Vector):
+        return Vector(self.multiply_i(v), self.multiply_j(v))
+
+    def det(self):
+        return self.ix * self.jy - self.iy * self.jx
+
+    def inverse(self):
+        d = self.det()
+        return Matrix(self.jy / d, -self.iy / d, -self.jx / d, self.ix / d)
+
+
+# represents an 2-dimensional rectangle
+class Rectangle:
+    def __init__(self, from_point: Vector, to_point: Vector):
+        self.bottom_left = Vector(min(from_point.x, to_point.x), min(from_point.y, to_point.y))
+        self.top_right = Vector(max(from_point.x, to_point.x), max(from_point.y, to_point.y))
+        self.bottom_right = Vector(self.top_right.x, self.bottom_left.y)
+        self.top_left = Vector(self.bottom_left.x, self.top_right.y)
+
+    def __add__(self, other: Vector):
+        return Rectangle(self.bottom_left + other, self.top_right + other)
+
+    def __sub__(self, other: Vector):
+        return Rectangle(self.bottom_left - other, self.top_right - other)
+
+    def __mul__(self, factor: float):
+        return Rectangle(self.bottom_left * factor, self.top_right * factor)
+
+    def __truediv__(self, factor: float):
+        return Rectangle(self.bottom_left / factor, self.top_right / factor)
+
+    # centre point of the rectangle
+    def centre(self):
+        return Vector((self.bottom_left.x + self.top_right.x) / 2.0,
+                      (self.bottom_left.y + self.top_right.y) / 2.0)
+
+    # point is on or within the boundary
+    def contains(self, point: Vector, tolerance: float):
+        return (self.bottom_left.x - tolerance <= point.x <= self.top_right.x + tolerance and
+                self.bottom_left.y - tolerance <= point.y <= self.top_right.y + tolerance)
+
+    # list of intersections with the rectangle boundary
+    def intersections_with(self, line: 'Line', tolerance: float):
+        boundaries = [
+            Line(self.bottom_left, self.top_left),
+            Line(self.bottom_right, self.top_right),
+            Line(self.bottom_left, self.bottom_right),
+            Line(self.top_left, self.top_right)]
+        return [intersection
+                for intersection in [boundary.intersection_with(line, tolerance, True) for boundary in boundaries]
+                if intersection is not None]
+
+
+# represents a line between two 2-dimensional points
+class Line:
+    def __init__(self, start_point: Vector, end_point: Vector):
+        self.start_point = start_point
+        self.end_point = end_point
+        self.bounds = Rectangle(start_point, end_point)
+        direction = end_point - start_point
+        self.unit_direction = direction / direction.magnitude()
+
+    def __add__(self, other: Vector):
+        return Line(self.start_point + other, self.end_point + other)
+
+    def __sub__(self, other: Vector):
+        return Line(self.start_point - other, self.end_point - other)
+
+    def __mul__(self, factor: float):
+        return Line(self.start_point * factor, self.end_point * factor)
+
+    def __truediv__(self, factor: float):
+        return Line(self.start_point / factor, self.end_point / factor)
+
+    def contains(self, point: Vector, tolerance: float, bounded: bool):
+        return self.is_parallel_to(Line(self.start_point, point), tolerance) and (
+                not bounded or self.bounds.contains(point, tolerance))
+
+    def is_parallel_to(self, other: 'Line', tolerance: float):
+        return 1.0 - abs(self.unit_direction.dot(other.unit_direction)) < tolerance
+
+    def intersection_with(self, other: 'Line', tolerance: float, bounded: bool):
+        if self.is_parallel_to(other, tolerance):
+            return None
+        d = Matrix(self.unit_direction.x, other.unit_direction.x, self.unit_direction.y, other.unit_direction.y)
+        lamb = d.inverse().multiply_i(other.start_point - self.start_point)
+        intersection = self.start_point + self.unit_direction * lamb
+        return (
+            intersection
+            if not bounded or (
+                            self.bounds.contains(intersection, tolerance) and
+                            other.bounds.contains(intersection, tolerance))
+            else None)
---
+++ tests/test_geometry.py
@@ -0,0 +1,300 @@
+import unittest
+from spacial.geometry import Vector, Matrix, Rectangle, Line
+
+
+tolerance: float = 1e-7
+
+
+class VectorTests(unittest.TestCase):
+    def test_init(self):
+        p = Vector(1.0, 2.0)
+        self.assertEqual(p.x, 1.0)
+        self.assertEqual(p.y, 2.0)
+
+    def test_add(self):
+        p1 = Vector(5.0, 3.0)
+        p2 = Vector(6.0, 1.0)
+        p3 = p2 + p1
+        self.assertTrue(p3.equals(Vector(11.0, 4.0), tolerance))
+
+    def test_sub(self):
+        p1 = Vector(5.0, 3.0)
+        p2 = Vector(6.0, 1.0)
+        p3 = p2 - p1
+        self.assertTrue(p3.equals(Vector(1.0, -2.0), tolerance))
+
+    def test_neg(self):
+        p1 = Vector(5.0, 3.0)
+        p2 = Vector(-5.0, -3.0)
+        p3 = -p1
+        self.assertTrue(p2.equals(p3, tolerance))
+
+    def test_mul(self):
+        p1 = Vector(5.0, 3.0)
+        p2 = Vector(15.0, 9.0)
+        p3 = p1 * 3.0
+        self.assertTrue(p2.equals(p3, tolerance))
+
+    def test_truediv(self):
+        p1 = Vector(15.0, 9.0)
+        p2 = Vector(5.0, 3.0)
+        p3 = p1 / 3.0
+        self.assertTrue(p2.equals(p3, tolerance))
+
+    def test_equals(self):
+        p1 = Vector(1.0, 2.0)
+        p2 = Vector(1.0 + tolerance, 2.0 - tolerance)
+        self.assertTrue(p1.equals(p2, tolerance * 2.0))
+
+    def test_dot(self):
+        p1 = Vector(0.0, 1.0)
+        p2 = Vector(1.0, 0.0)
+        self.assertEqual(p1.dot(p2), 0.0)
+        self.assertEqual(p1.dot(p1), 1.0)
+        self.assertEqual(p2.dot(p2), 1.0)
+
+    def test_magnitude(self):
+        self.assertEqual(Vector(3.0, 4.0).magnitude(), 5.0)
+
+
+class MatrixTests(unittest.TestCase):
+    def test_init(self):
+        m = Matrix(0.0, 1.0, 2.0, 3.0)
+        self.assertEqual(m.ix, 0.0)
+        self.assertEqual(m.iy, 1.0)
+        self.assertEqual(m.jx, 2.0)
+        self.assertEqual(m.jy, 3.0)
+
+    def test_eq(self):
+        m1 = Matrix(1.0, 2.0, 3.0, 4.0)
+        m2 = Matrix(1.0, 2.0, 3.0, 4.0)
+        m3 = Matrix(4.0, 3.0, 2.0, 1.0)
+        self.assertEqual(m1, m2)
+        self.assertNotEqual(m2, m3)
+
+    def test_add(self):
+        m1 = Matrix(5.0, 3.0, 1.0, -1.0)
+        m2 = Matrix(6.0, 1.0, 4.0, 2.0)
+        m3 = m2 + m1
+        self.assertEqual(m3, Matrix(11.0, 4.0, 5.0, 1.0))
+
+    def test_sub(self):
+        m1 = Matrix(6.0, 1.0, 4.0, 2.0)
+        m2 = Matrix(11.0, 4.0, 5.0, 1.0)
+        m3 = m2 - m1
+        self.assertEqual(m3, Matrix(5.0, 3.0, 1.0, -1.0))
+
+    def test_neg(self):
+        m1 = Matrix(6.0, 1.0, 4.0, 2.0)
+        m2 = -m1
+        self.assertEqual(m2, Matrix(-6.0, -1.0, -4.0, -2.0))
+
+    def test_mul(self):
+        m1 = Matrix(6.0, 1.0, 4.0, 2.0)
+        m2 = m1 * 2.0
+        self.assertEqual(m2, Matrix(12.0, 2.0, 8.0, 4.0))
+
+    def test_truediv(self):
+        m1 = Matrix(6.0, 1.0, 4.0, 2.0)
+        m2 = m1 / 2.0
+        self.assertEqual(m2, Matrix(3.0, 0.5, 2.0, 1.0))
+
+    def test_multiply(self):
+        m = Matrix(0.0, 1.0, 1.0, 0.0)
+        p1 = Vector(0.0, 1.0)
+        p2 = Vector(1.0, 0.0)
+        self.assertTrue(m.multiply(p1).equals(p2, tolerance))
+
+    def test_det(self):
+        m = Matrix(1.0, 2.0, 3.0, 4.0)
+        self.assertEqual(m.det(), -2.0)
+
+    def test_inverse(self):
+        m = Matrix(1.0, 1.0, 1.0, 3.0)
+        i = Matrix(1.5, -0.5, -0.5, 0.5)
+        self.assertEqual(m.inverse(), i)
+
+
+class RectangleTests(unittest.TestCase):
+    def test_init(self):
+        bl = Vector(-2.0, -3.0)
+        tr = Vector(1.0, 4.0)
+        r = Rectangle(bl, tr)
+        self.assertTrue(r.bottom_left.equals(bl, tolerance))
+        self.assertTrue(r.top_right.equals(tr, tolerance))
+        self.assertTrue(r.bottom_right.equals(Vector(tr.x, bl.y), tolerance))
+        self.assertTrue(r.top_left.equals(Vector(bl.x, tr.y), tolerance))
+
+    def test_add(self):
+        r = Rectangle(Vector(-2.0, -3.0), Vector(1.0, 4.0))
+        ofs = Vector(1.0, 2.0)
+        r_add = r + ofs
+        self.assertTrue(r_add.bottom_left.equals(r.bottom_left + ofs, tolerance))
+        self.assertTrue(r_add.top_right.equals(r.top_right + ofs, tolerance))
+        self.assertTrue(r_add.bottom_right.equals(r.bottom_right + ofs, tolerance))
+        self.assertTrue(r_add.top_left.equals(r.top_left + ofs, tolerance))
+
+    def test_sub(self):
+        r = Rectangle(Vector(-2.0, -3.0), Vector(1.0, 4.0))
+        ofs = Vector(1.0, 2.0)
+        r_sub = r - ofs
+        self.assertTrue(r_sub.bottom_left.equals(r.bottom_left - ofs, tolerance))
+        self.assertTrue(r_sub.top_right.equals(r.top_right - ofs, tolerance))
+        self.assertTrue(r_sub.bottom_right.equals(r.bottom_right - ofs, tolerance))
+        self.assertTrue(r_sub.top_left.equals(r.top_left - ofs, tolerance))
+
+    def test_mul(self):
+        r = Rectangle(Vector(-2.0, -3.0), Vector(1.0, 4.0))
+        factor = 3.0
+        r_mul = r * factor
+        self.assertTrue(r_mul.bottom_left.equals(r.bottom_left * factor, tolerance))
+        self.assertTrue(r_mul.top_right.equals(r.top_right * factor, tolerance))
+        self.assertTrue(r_mul.bottom_right.equals(r.bottom_right * factor, tolerance))
+        self.assertTrue(r_mul.top_left.equals(r.top_left * factor, tolerance))
+
+    def test_truediv(self):
+        r = Rectangle(Vector(-2.0, -3.0), Vector(1.0, 4.0))
+        factor = 3.0
+        r_mul = r / factor
+        self.assertTrue(r_mul.bottom_left.equals(r.bottom_left / factor, tolerance))
+        self.assertTrue(r_mul.top_right.equals(r.top_right / factor, tolerance))
+        self.assertTrue(r_mul.bottom_right.equals(r.bottom_right / factor, tolerance))
+        self.assertTrue(r_mul.top_left.equals(r.top_left / factor, tolerance))
+
+    def test_contains(self):
+        bl = Vector(-2.0, -3.0)
+        tr = Vector(1.0, 4.0)
+        r = Rectangle(bl, tr)
+        # inside
+        self.assertTrue(r.contains(Vector(0.0, 0.0), tolerance))
+        # boundary
+        self.assertTrue(r.contains(Vector(1.0, 0.0), tolerance))
+        self.assertTrue(r.contains(Vector(0.0, 4.0), tolerance))
+        self.assertTrue(r.contains(Vector(-2.0, 0.0), tolerance))
+        self.assertTrue(r.contains(Vector(0.0, -3.0), tolerance))
+        # outside
+        self.assertFalse(r.contains(Vector(2.0, 0.0), tolerance))
+        self.assertFalse(r.contains(Vector(0.0, 5.0), tolerance))
+        self.assertFalse(r.contains(Vector(-3.0, 0.0), tolerance))
+        self.assertFalse(r.contains(Vector(0.0, -4.0), tolerance))
+
+    def test_intersections_with(self):
+        a_bit = Vector(0.5, 0.5)
+        r = Rectangle(Vector(-1.0, -1.0), Vector(1.0, 1.0))
+
+        # line entirely within the rectangle
+        l_inside = Line(r.bottom_left + a_bit, r.top_right - a_bit)
+        self.assertEqual(len(r.intersections_with(l_inside, tolerance)), 0)
+
+        # line from centre through left boundary
+        l_left = Line(r.centre(), r.top_left - a_bit)
+        self.assertEqual(len(r.intersections_with(l_left, tolerance)), 1)
+
+        # line from centre through right boundary
+        l_right = Line(r.centre(), r.bottom_right + a_bit)
+        self.assertEqual(len(r.intersections_with(l_right, tolerance)), 1)
+
+        # line from centre through top boundary
+        l_top = Line(r.centre(), r.top_left - a_bit)
+        self.assertEqual(len(r.intersections_with(l_top, tolerance)), 1)
+
+        # line from centre through bottom boundary
+        l_bottom = Line(r.centre(), r.bottom_right + a_bit)
+        self.assertEqual(len(r.intersections_with(l_bottom, tolerance)), 1)
+
+        # line through left and top boundary
+        l_left_top = Line(r.top_left - a_bit, r.top_right + a_bit)
+        self.assertEqual(len(r.intersections_with(l_left_top, tolerance)), 2)
+
+        # line through bottom and left boundary
+        l_bottom_left = Line(r.bottom_right - a_bit, r.top_left - a_bit)
+        self.assertEqual(len(r.intersections_with(l_bottom_left, tolerance)), 2)
+
+        # line through right and bottom boundary
+        l_right_bottom = Line(r.top_right + a_bit, r.bottom_left + a_bit)
+        self.assertEqual(len(r.intersections_with(l_right_bottom, tolerance)), 2)
+
+        # line through top and right boundary
+        l_top_right = Line(r.top_left + a_bit, r.bottom_right + a_bit)
+        self.assertEqual(len(r.intersections_with(l_top_right, tolerance)), 2)
+
+        # line through left and right boundary
+        l_left_right = Line(r.top_left - a_bit, r.bottom_right + a_bit)
+        self.assertEqual(len(r.intersections_with(l_left_right, tolerance)), 2)
+
+        # line through top and bottom boundary
+        l_top_bottom = Line(r.top_left + a_bit, r.bottom_right - a_bit)
+        self.assertEqual(len(r.intersections_with(l_left_right, tolerance)), 2)
+
+
+class LineTests(unittest.TestCase):
+    def test_init(self):
+        s = Vector(1.0, 2.0)
+        e = Vector(3.0, 4.0)
+        ln = Line(s, e)
+        self.assertEqual(ln.start_point, s)
+        self.assertEqual(ln.end_point, e)
+
+    def test_add(self):
+        l_before = Line(Vector(-2.0, -3.0), Vector(1.0, 4.0))
+        ofs = Vector(1.0, 2.0)
+        l_add = l_before + ofs
+        self.assertTrue(l_add.start_point.equals(l_before.start_point + ofs, tolerance))
+        self.assertTrue(l_add.end_point.equals(l_before.end_point + ofs, tolerance))
+
+    def test_sub(self):
+        l_before = Line(Vector(-2.0, -3.0), Vector(1.0, 4.0))
+        ofs = Vector(1.0, 2.0)
+        l_sub = l_before - ofs
+        self.assertTrue(l_sub.start_point.equals(l_before.start_point - ofs, tolerance))
+        self.assertTrue(l_sub.end_point.equals(l_before.end_point - ofs, tolerance))
+
+    def test_mul(self):
+        l_before = Line(Vector(-2.0, -3.0), Vector(1.0, 4.0))
+        factor = 3.0
+        l_add = l_before * factor
+        self.assertTrue(l_add.start_point.equals(l_before.start_point * factor, tolerance))
+        self.assertTrue(l_add.end_point.equals(l_before.end_point * factor, tolerance))
+
+    def test_truediv(self):
+        l_before = Line(Vector(-2.0, -3.0), Vector(1.0, 4.0))
+        factor = 3.0
+        l_add = l_before / factor
+        self.assertTrue(l_add.start_point.equals(l_before.start_point / factor, tolerance))
+        self.assertTrue(l_add.end_point.equals(l_before.end_point / factor, tolerance))
+
+    def test_contains(self):
+        l1 = Line(Vector(0.0, 0.0), Vector(3.0, 4.0))
+        self.assertTrue(l1.contains(Vector(1.5, 2.0), tolerance, True))
+        self.assertFalse(l1.contains(Vector(2.0, 1.5), tolerance, True))
+        self.assertFalse(l1.contains(Vector(4.5, 6.0), tolerance, True))
+        self.assertFalse(l1.contains(Vector(6.0, 4.5), tolerance, True))
+        self.assertTrue(l1.contains(Vector(4.5, 6.0), tolerance, False))
+        self.assertFalse(l1.contains(Vector(6.0, 4.5), tolerance, False))
+
+    def test_is_parallel_to(self):
+        l1 = Line(Vector(3.0, 4.0), Vector(4.0, 6.0))
+        l2 = Line(Vector(-1.0, 1.0), Vector(-0.5, 2.0))
+        l3 = Line(Vector(3.0, 4.0), Vector(2.0, 2.0))
+        self.assertTrue(l1.is_parallel_to(l2, tolerance))
+        self.assertTrue(l2.is_parallel_to(l3, tolerance))
+
+    def test_intersection_with(self):
+        l1 = Line(Vector(0.0, 0.0), Vector(1.0, 1.0))
+        l2 = Line(Vector(0.0, 1.0), Vector(1.0, 0.0))
+        l3 = Line(Vector(1.0, 1.0), Vector(2.0, 0.0))
+        l4 = Line(Vector(-2.0, 3.0), Vector(-1.0, 2.0))
+
+        # lines intersecting between their end-points
+        self.assertTrue(l1.intersection_with(l2, tolerance, True).equals(Vector(0.5, 0.5), tolerance))
+
+        # parallel lines (not intersecting)
+        self.assertIsNone(l2.intersection_with(l3, tolerance, False))
+
+        # lines intersecting outside their end-points
+        self.assertTrue(l1.intersection_with(l4, tolerance, False).equals(Vector(0.5, 0.5), tolerance))
+        self.assertIsNone(l1.intersection_with(l4, tolerance, True))
+
+
+if __name__ == '__main__':
+    unittest.main()
